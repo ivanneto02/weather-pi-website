670d1d3e7087ae611da3f0bfb0da6d9473ceb11c
Ivan Neto <ivan.a.neto@hotmail.com>
2025-08-22 22:12:43 -0700

    Smaller Header Size

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,7 +2,7 @@
     import HeaderLink from "$lib/components/HeaderLink.svelte";
 </script>
 
-<div class="text-xl h-20 bg-inherit flex gap-7 flex-row justify-center border-solid border-indigo-900 border-b-2">
+<div class="text-lg h-20 bg-inherit flex gap-5 flex-row justify-center border-solid border-indigo-900 border-b-2">
     <HeaderLink to="/" text={"Home"} icon={"line-md:home"}/>
     <HeaderLink to="/about" text={"About"} icon={"line-md:question"}/>
     <HeaderLink to="/building-this-website" text={"Website"} icon={"pixel:technology"}/>
