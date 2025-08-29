8ca68f262f4adb85e5d7c4ab59096262c1a363dc
Ivan Neto <ivan.a.neto@hotmail.com>
2025-08-29 02:34:28 -0700

    Adding air quality data information

--- data-programs/chunk_batch_write_command.js
+++ data-programs/chunk_batch_write_command.js
@@ -1,31 +1,37 @@
 
 let chunkSize = 3;
 
+let flattenedSeriesData = [];
+
 let data = [
-    { index: 1, value: 2 },
-    { index: 2, value: 2 },
-    { index: 3, value: 2 },
-    { index: 4, value: 2 },
-    { index: 5, value: 2 },
-    { index: 6, value: 2 },
-    { index: 7, value: 2 },
-    { index: 8, value: 2 },
-    { index: 9, value: 2 },
-    { index: 10, value: 2 },
-    { index: 11, value: 2 },
-    { index: 12, value: 2 },
-    { index: 13, value: 2 },
-    { index: 14, value: 2 },
-    { index: 15, value: 2 },
-    { index: 16, value: 2 },
-    { index: 17, value: 2 },
-    { index: 18, value: 2 },
-    { index: 19, value: 2 },
+    { index: 1, value1: 3, value2: 4 },
+    { index: 2, value1: 3, value2: 4 },
+    { index: 3, value1: 3, value2: 4 },
+    { index: 4, value1: 3, value2: 4 },
+    { index: 5, value1: 3, value2: 4 },
+    { index: 6, value1: 3, value2: 4 },
+    { index: 7, value1: 3, value2: 4 },
+    { index: 8, value1: 3, value2: 4 },
+    { index: 9, value1: 3, value2: 4 },
+    { index: 10, value1: 3, value2: 4 },
+    { index: 11, value1: 3, value2: 4 },
+    { index: 12, value1: 3, value2: 4 },
+    { index: 13, value1: 3, value2: 4 },
+    { index: 14, value1: 3, value2: 4 },
+    { index: 15, value1: 3, value2: 4 },
+    { index: 16, value1: 3, value2: 4 },
+    { index: 17, value1: 3, value2: 4 },
+    { index: 18, value1: 3, value2: 4 },
+    { index: 19, value1: 3, value2: 4 },
 ]
 
-let chunkNumber = 1;
-for (let i = 0; i < data.length; i += chunkSize) {
-    console.log(chunkNumber);
-    chunkNumber += 1;
-    console.log(data.slice(i, i + chunkSize));
-}
+const values = ["value1", "value2"];
+
+data.map((d) => {
+    values.map((c) => {
+        flattenedSeriesData.push({ value: d[c], type: c });
+    });
+});
+
+console.log(flattenedSeriesData);
+
