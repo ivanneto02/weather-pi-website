3e3d2d35630cbdf2922498483255fdcf4a6a64fc
Ivan Neto <ivan.a.neto@hotmail.com>
2025-08-29 17:31:17 -0700

    Finished Data Gathering page

--- src/routes/data-gathering/+page.svelte
+++ src/routes/data-gathering/+page.svelte
@@ -1,5 +1,7 @@
 <h1>Gathering Data</h1>
 
+<br />
+
 <h2>Real-Time Information</h2>
 
 <p>
@@ -10,26 +12,68 @@
 
 <p>The data you see displayed in the homepage is real-time and gathered 10 seconds ago, at most.</p>
 
-<h2>Serverless Architecture</h2>
-
-<p>
-	AWS DynamoDB was used to store time-series sensor data from the Raspberry Pi. An AWS API Gateway
-	was utilized to keep the entire pipeline serverless. Lambda functions were used to format and
-	store, as well as query the data from DynamoDB.
-</p>
-
+<br />
 <h2>Crash-redundant Systems Programming</h2>
 
+<h3>Crash recovery</h3>
 <p>
 	The Raspberry Pi runs a Raspberry Pi OS Lite to keep a lower CPU and memory load, and several
 	scripts and systemd services were created to recover from crashes. All of this results in a 24-7
 	data-gathering effort that can be implemented anywhere.
 </p>
 
-<h2>Air Quality Data Format</h2>
+<p>
+	Crashes and systemctl service stops causes an immediate Python script restart, and the data
+	gathering process resumes. The Raspberry Pi is connected to a Uninterruptible Power Supply (UPS)
+	to prevent loss of power.
+</p>
 
-<p>TODO</p>
+<p>
+	Lack of connection errors are handled through a robust Python, Exception-based programming.
+	Exceptions are carefully placed so that samples remain in-memory throughout a lack of connection
+	error.
+</p>
 
-<h2>Temperature, Humidity, Pressure Data Format</h2>
+<h3>DynamoDB Unproccessed Data</h3>
+<p>
+	The AWS Lambda function that uploads data to DynamoDB has built-in redundancy handled through an
+	exponential "back off" method to upload data to DynamoDB in 25-sized BatchWriteCommand PUT
+	requests. If any failure occurs in the upload, my lambda function retries the upload 5 times,
+	waiting in exponential intervals (100ms, 200ms, 400ms, 800ms, ...). This ensures greatly improved
+	chances that the upload request is successful.
+</p>
 
-<p>TODO</p>
+<br />
+<h2>Data Format</h2>
+
+<p>The BME280 sensor gathers temperature, humidity, and pressure data, once per sample</p>
+
+<p>BME280 data format:</p>
+
+<ul>
+	<li>temperature: float</li>
+	<li>humidity: float</li>
+	<li>pressure: float</li>
+</ul>
+
+<p>PMS5003 data format:</p>
+
+<ul>
+	<li>count_03: int</li>
+	<li>count_05: int</li>
+	<li>count_10: int</li>
+	<li>count_25: int</li>
+	<li>count_50: int</li>
+	<li>PM1.0 factory: int</li>
+	<li>PM2.5 factory: int</li>
+	<li>PM10.0 factory: int</li>
+	<li>PM1.0 outside: int</li>
+	<li>PM2.5 outside: int</li>
+	<li>PM10.0 outside: int</li>
+</ul>
+
+<p>
+	Note: "factory" vs "outside" refers to the sensor calibration. Factory calibration is apt for
+	indoor use, and "outside" calibration is for outdoor environments. This Raspberry Pi sits indoors,
+	so the "factory" measurement is more accurate.
+</p>
