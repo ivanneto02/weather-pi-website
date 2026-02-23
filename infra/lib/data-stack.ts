503400cf90895b55e3624fcbcb05a9539af0b0a7
Ivan Neto <ivan.a.neto@hotmail.com>
2026-02-22 17:26:45 -0800

    feat: increasing number of points for resolution

--- infra/lib/data-stack.ts
+++ infra/lib/data-stack.ts
@@ -3,11 +3,32 @@ import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as iam from "aws-cdk-lib/aws-iam";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as events from "aws-cdk-lib/aws-events";
+import * as targets from "aws-cdk-lib/aws-events-targets";
 import { Construct } from 'constructs';
+
+const WINDOW_SCHEDULES = [
+    { window: "1h",  rate: events.Schedule.rate(cdk.Duration.minutes(5)) },
+    { window: "10h", rate: events.Schedule.rate(cdk.Duration.minutes(10)) },
+    { window: "1d",  rate: events.Schedule.rate(cdk.Duration.minutes(30)) },
+    { window: "10d", rate: events.Schedule.rate(cdk.Duration.hours(2)) },
+    { window: "3m",  rate: events.Schedule.rate(cdk.Duration.hours(6)) },
+    { window: "12m", rate: events.Schedule.rate(cdk.Duration.hours(12)) },
+];
 
 export class WeatherPIDataStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
+        // --------- PRE-COMPUTED DATA BUCKET --------------
+
+        const precomputedBucket = new s3.Bucket(this, "WeatherPIPrecomputedDataBucket", {
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            lifecycleRules: [{ expiration: cdk.Duration.days(30) }],
+        });
 
         // --------- HUMIDITY, TEMPERATURE, PRESSURE --------------
 
@@ -43,12 +64,17 @@ export class WeatherPIDataStack extends cdk.Stack {
                 runtime: lambda.Runtime.NODEJS_LATEST,
                 handler: "index.handler",
                 timeout: cdk.Duration.seconds(15),
+                environment: {
+                    BUCKET_NAME: precomputedBucket.bucketName,
+                    S3_PREFIX: "htp",
+                },
             }
         );
 
         // allow the functions to modify DynamoDB
         humidityTemperaturePressureDataTable.grantWriteData(humidityTemperaturePressureUploadFunction);
         humidityTemperaturePressureDataTable.grantReadData(humidityTemperaturePressureDownloadFunction);
+        precomputedBucket.grantRead(humidityTemperaturePressureDownloadFunction);
 
         // api function deploys lambda
         const humidityTemperaturePressureAPI: apigateway.RestApi = new apigateway.RestApi(
@@ -58,6 +84,34 @@ export class WeatherPIDataStack extends cdk.Stack {
                 cloudWatchRole: false
             }
         );
+
+        // HTP cron Lambda
+        const htpCronFunction = new lambda.Function(this, "WeatherPIHtpCronFunction", {
+            code: lambda.Code.fromAsset("lib/functions/htpCronFunction"),
+            runtime: lambda.Runtime.NODEJS_LATEST,
+            handler: "index.handler",
+            timeout: cdk.Duration.seconds(30),
+            memorySize: 256,
+            environment: {
+                TABLE_NAME: humidityTemperaturePressureDataTable.tableName,
+                BUCKET_NAME: precomputedBucket.bucketName,
+                S3_PREFIX: "htp",
+                SAMPLE_COUNT: "600",
+            },
+        });
+
+        humidityTemperaturePressureDataTable.grantReadData(htpCronFunction);
+        precomputedBucket.grantWrite(htpCronFunction);
+
+        // EventBridge rules for HTP cron
+        for (const { window, rate } of WINDOW_SCHEDULES) {
+            const rule = new events.Rule(this, `HtpCron-${window}`, {
+                schedule: rate,
+            });
+            rule.addTarget(new targets.LambdaFunction(htpCronFunction, {
+                event: events.RuleTargetInput.fromObject({ window }),
+            }));
+        }
 
 
         // -------- AIR QUALITY -----------
@@ -94,12 +148,17 @@ export class WeatherPIDataStack extends cdk.Stack {
                 runtime: lambda.Runtime.NODEJS_LATEST,
                 handler: "index.handler",
                 timeout: cdk.Duration.seconds(15),
+                environment: {
+                    BUCKET_NAME: precomputedBucket.bucketName,
+                    S3_PREFIX: "aq",
+                },
             }
         );
 
         // allow the functions to modify DynamoDB
         airQualityDataTable.grantWriteData(airQualityUploadFunction);
         airQualityDataTable.grantReadData(airQualityDownloadFunction);
+        precomputedBucket.grantRead(airQualityDownloadFunction);
 
         // api function deploys lambda
         const airQualityAPI: apigateway.RestApi = new apigateway.RestApi(
@@ -109,6 +168,34 @@ export class WeatherPIDataStack extends cdk.Stack {
                 cloudWatchRole: false,
             }
         );
+
+        // AQ cron Lambda
+        const aqCronFunction = new lambda.Function(this, "WeatherPIAqCronFunction", {
+            code: lambda.Code.fromAsset("lib/functions/aqCronFunction"),
+            runtime: lambda.Runtime.NODEJS_LATEST,
+            handler: "index.handler",
+            timeout: cdk.Duration.seconds(30),
+            memorySize: 256,
+            environment: {
+                TABLE_NAME: airQualityDataTable.tableName,
+                BUCKET_NAME: precomputedBucket.bucketName,
+                S3_PREFIX: "aq",
+                SAMPLE_COUNT: "600",
+            },
+        });
+
+        airQualityDataTable.grantReadData(aqCronFunction);
+        precomputedBucket.grantWrite(aqCronFunction);
+
+        // EventBridge rules for AQ cron
+        for (const { window, rate } of WINDOW_SCHEDULES) {
+            const rule = new events.Rule(this, `AqCron-${window}`, {
+                schedule: rate,
+            });
+            rule.addTarget(new targets.LambdaFunction(aqCronFunction, {
+                event: events.RuleTargetInput.fromObject({ window }),
+            }));
+        }
 
         // API Resources
         const airQualityAPIResource = airQualityAPI.root.addResource("samples");
