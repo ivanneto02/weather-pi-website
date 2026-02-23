78cab54be8aa4a414e0f7e504977b36e97abfe9a
Ivan Neto <ivan.a.neto@hotmail.com>
2026-02-22 16:34:00 -0800

    chore: moving URLs for IoT data fetching to single source

---
+++ src/lib/config/endpoints.ts
@@ -0,0 +1,2 @@
+export const HTP_ENDPOINT = "https://93xdazuw09.execute-api.us-west-1.amazonaws.com/prod/samples";
+export const AQ_ENDPOINT = "https://jbn6u8l8h7.execute-api.us-west-1.amazonaws.com/prod/samples";
